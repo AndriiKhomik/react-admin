dab1f3a3d295112d77f05e09c347ff5793820b0c
Andrii <andrii.khomik@gmail.com>
2023-06-23 22:28:34 +0300

    add faq page

--- src/sceens/FAQ/index.jsx
+++ src/sceens/FAQ/index.jsx
@@ -1,7 +1,101 @@
 import React from "react";
+import { useTheme } from "@emotion/react";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Qustion Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur eligendi numquam accusamus itaque illo sunt quis
+            nostrum repellat nesciunt eum? Inventore voluptatibus quod maiores
+            qui libero veniam officia a nostrum. Molestias illum harum mollitia
+            dolores suscipit sit voluptates eos quia distinctio animi voluptas,
+            eligendi assumenda nihil vero, magni natus! Distinctio, nobis
+            commodi? Temporibus necessitatibus modi consequatur non! Adipisci,
+            cupiditate officia.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another important question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur eligendi numquam accusamus itaque illo sunt quis
+            nostrum repellat nesciunt eum? Inventore voluptatibus quod maiores
+            qui libero veniam officia a nostrum. Molestias illum harum mollitia
+            dolores suscipit sit voluptates eos quia distinctio animi voluptas,
+            eligendi assumenda nihil vero, magni natus! Distinctio, nobis
+            commodi? Temporibus necessitatibus modi consequatur non! Adipisci,
+            cupiditate officia.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your favorite question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur eligendi numquam accusamus itaque illo sunt quis
+            nostrum repellat nesciunt eum? Inventore voluptatibus quod maiores
+            qui libero veniam officia a nostrum. Molestias illum harum mollitia
+            dolores suscipit sit voluptates eos quia distinctio animi voluptas,
+            eligendi assumenda nihil vero, magni natus! Distinctio, nobis
+            commodi? Temporibus necessitatibus modi consequatur non! Adipisci,
+            cupiditate officia.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Final question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur eligendi numquam accusamus itaque illo sunt quis
+            nostrum repellat nesciunt eum? Inventore voluptatibus quod maiores
+            qui libero veniam officia a nostrum. Molestias illum harum mollitia
+            dolores suscipit sit voluptates eos quia distinctio animi voluptas,
+            eligendi assumenda nihil vero, magni natus! Distinctio, nobis
+            commodi? Temporibus necessitatibus modi consequatur non! Adipisci,
+            cupiditate officia.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
 };
 
 export default FAQ;
