e1239d4308168f13b7c0e12bc29d5a8658771a65
Andrii <andrii.khomik@gmail.com>
2023-06-24 14:11:17 +0300

    implement charts

--- src/sceens/BarPage/index.jsx
+++ src/sceens/BarPage/index.jsx
@@ -1,7 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
+import BarChart from "../../components/BarChart";
+import Header from "../../components/Header";
 
 const BarPage = () => {
-  return <div>Bar</div>;
+  return (
+    <Box m="20px">
+      <Header title="BAR CHART" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default BarPage;
